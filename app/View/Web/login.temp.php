e2139641914b9a47314dd20121f276cff6ff00b4
Frano Sasvari <sasvari.frano@gmail.com>
2017-02-03 15:45:37 +0100

    Change styles and translations in login view

--- app/View/Web/login.temp.php
+++ app/View/Web/login.temp.php
@@ -10,17 +10,17 @@ $canonical = $lang->get('routes.login');
 	<h1><?php echo $lang->get('page.login.heading'); ?></h1>
 
 	<div class='form-group'>
-		<label for='email' class='label'>E-mail address:</label>
+		<label for='email' class='label'><?php echo $lang->get('form.login.email'); ?>:</label>
 		<div class='input-group'>
-			<input type='text' name='email' id='email' value='<?php echo (isset($post_data['email']) ? $post_data['email'] : ''); ?>' placeholder='E-mail adress' maxlength='80' class='form-input'>
+			<input type='text' name='email' id='email' value='<?php echo (isset($post_data['email']) ? $post_data['email'] : ''); ?>' placeholder='<?php echo $lang->get('form.login.email'); ?>' maxlength='80' class='form-control'>
 			<span class='input-group-addon'><i class='fa fa-envelope'></i></span>
 		</div>
 	</div>
 
 	<div class='form-group'>
-		<label for='password' class='label'>Password:</label>
+		<label for='password' class='label'><?php echo $lang->get('form.login.password'); ?>:</label>
 		<div class='input-group'>
-			<input type='text' name='password' id='password' value='' placeholder='Password' maxlength='30' class='form-input'>
+			<input type='text' name='password' id='password' value='' placeholder='<?php echo $lang->get('form.login.password'); ?>' maxlength='30' class='form-control'>
 			<span class='input-group-addon'><i class='fa fa-lock'></i></span>
 		</div>
 	</div>
