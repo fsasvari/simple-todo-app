760ea1edcfb7fae4a119077b50cb5cdabd0ae601
Frano Sasvari <sasvari.frano@gmail.com>
2017-02-03 13:24:49 +0100

    Change default controller

--- app/Controller/Controller.php
+++ app/Controller/Controller.php
@@ -3,8 +3,30 @@
 namespace App\Controller;
 
 use Slinky\Routing\Controller as BaseController;
+use Slinky\Http\Response;
+use Slinky\Translation\Language;
 
 class Controller extends BaseController
 {
-	
+	/**
+	 * The language instance
+	 * 
+	 * @var \Slinky\Translation\Language
+	 */
+	protected $lang;
+
+	/**
+	 * Create new controller instance
+	 *
+	 * @param \Slinky\Http\Response $response
+	 * @param \Slinky\Translation\Language $lang
+	 * @return void
+	 */
+	public function __construct(Response $response, Language $lang)
+	{
+		parent::__construct($response);
+
+		$this->lang = $lang;
+		$this->response->lang = $lang;
+	}
 }
