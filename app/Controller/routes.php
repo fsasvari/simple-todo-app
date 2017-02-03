60a091f6d02e503948ce665e9fa78bed7d9f8bf1
Frano Sasvari <sasvari.frano@gmail.com>
2017-02-03 15:30:39 +0100

    Create logout and register routes

--- app/Controller/routes.php
+++ app/Controller/routes.php
@@ -7,6 +7,7 @@ $auth = $app->getContainer()->get('Auth');
 // user logged in
 if ($auth->check()) {
 	$router->get('', 'List', 'index');
+	$router->get($lang->get('routes.logout'), 'Auth', 'logout');
 }
 
 // user logged out
@@ -14,5 +15,7 @@ else {
 	$router->get('', 'Auth', 'login');
 	$router->get($lang->get('routes.login'), 'Auth', 'login');
 	$router->post($lang->get('routes.login'), 'Auth', 'loginPost');
+	$router->get($lang->get('routes.register'), 'Auth', 'register');
+	$router->post($lang->get('routes.register'), 'Auth', 'registerPost');
 }
 
