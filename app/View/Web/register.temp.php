edee31d9ebbc1182ced7a41ee7475ae4ae9703fc
Frano Sasvari <sasvari.frano@gmail.com>
2017-02-03 15:45:24 +0100

    Change styles in register view

--- app/View/Web/register.temp.php
+++ app/View/Web/register.temp.php
@@ -12,7 +12,7 @@ $canonical = $lang->get('routes.register');
 	<div class='form-group'>
 		<label for='email' class='label'><?php echo $lang->get('form.register.email'); ?>:</label>
 		<div class='input-group'>
-			<input type='text' name='email' id='email' value='<?php echo (isset($post_data['email']) ? $post_data['email'] : ''); ?>' placeholder='E-mail adress' maxlength='80' class='form-input'>
+			<input type='text' name='email' id='email' value='<?php echo (isset($post_data['email']) ? $post_data['email'] : ''); ?>' placeholder='E-mail adress' maxlength='80' class='form-control'>
 			<span class='input-group-addon'><i class='fa fa-envelope'></i></span>
 		</div>
 	</div>
@@ -20,7 +20,7 @@ $canonical = $lang->get('routes.register');
 	<div class='form-group'>
 		<label for='password' class='label'><?php echo $lang->get('form.register.password'); ?>:</label>
 		<div class='input-group'>
-			<input type='text' name='password' id='password' value='' placeholder='<?php echo $lang->get('form.register.password_repeat'); ?>' maxlength='30' class='form-input'>
+			<input type='text' name='password' id='password' value='' placeholder='<?php echo $lang->get('form.register.password_repeat'); ?>' maxlength='30' class='form-control'>
 			<span class='input-group-addon'><i class='fa fa-lock'></i></span>
 		</div>
 	</div>
@@ -28,7 +28,7 @@ $canonical = $lang->get('routes.register');
 	<div class='form-group'>
 		<label for='password_repeat' class='label'><?php echo $lang->get('form.register.password_repeat'); ?>:</label>
 		<div class='input-group'>
-			<input type='text' name='password_repeat' id='password_repeat' value='' placeholder='<?php echo $lang->get('form.register.password_repeat'); ?>' maxlength='30' class='form-input'>
+			<input type='text' name='password_repeat' id='password_repeat' value='' placeholder='<?php echo $lang->get('form.register.password_repeat'); ?>' maxlength='30' class='form-control'>
 			<span class='input-group-addon'><i class='fa fa-lock'></i></span>
 		</div>
 	</div>
@@ -36,7 +36,7 @@ $canonical = $lang->get('routes.register');
 	<div class='form-group'>
 		<label for='firstname' class='label'><?php echo $lang->get('form.register.firstname'); ?>:</label>
 		<div class='input-group'>
-			<input type='text' name='firstname' id='firstname' value='' placeholder='<?php echo $lang->get('form.register.firstname'); ?>' maxlength='30' class='form-input'>
+			<input type='text' name='firstname' id='firstname' value='' placeholder='<?php echo $lang->get('form.register.firstname'); ?>' maxlength='50' class='form-control'>
 			<span class='input-group-addon'><i class='fa fa-user'></i></span>
 		</div>
 	</div>
@@ -44,7 +44,7 @@ $canonical = $lang->get('routes.register');
 	<div class='form-group'>
 		<label for='lastname' class='label'><?php echo $lang->get('form.register.lastname'); ?>:</label>
 		<div class='input-group'>
-			<input type='text' name='lastname' id='lastname' value='' placeholder='<?php echo $lang->get('form.register.lastname'); ?>' maxlength='30' class='form-input'>
+			<input type='text' name='lastname' id='lastname' value='' placeholder='<?php echo $lang->get('form.register.lastname'); ?>' maxlength='80' class='form-control'>
 			<span class='input-group-addon'><i class='fa fa-users'></i></span>
 		</div>
 	</div>
