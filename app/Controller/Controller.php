f062cf812c1fdea90b7275e661ebc41bfdb318a0
Frano Sasvari <sasvari.frano@gmail.com>
2017-02-03 13:25:45 +0100

    Add Request to Base controller

--- app/Controller/Controller.php
+++ app/Controller/Controller.php
@@ -4,13 +4,21 @@ namespace App\Controller;
 
 use Slinky\Routing\Controller as BaseController;
 use Slinky\Http\Response;
+use Slinky\Http\Request;
 use Slinky\Translation\Language;
 
 class Controller extends BaseController
 {
 	/**
 	 * The language instance
-	 * 
+	 *
+	 * @var \Slinky\Http\Request
+	 */
+	protected $request;
+
+	/**
+	 * The language instance
+	 *
 	 * @var \Slinky\Translation\Language
 	 */
 	protected $lang;
@@ -19,13 +27,15 @@ class Controller extends BaseController
 	 * Create new controller instance
 	 *
 	 * @param \Slinky\Http\Response $response
+	 * @param \Slinky\Http\Request $request
 	 * @param \Slinky\Translation\Language $lang
 	 * @return void
 	 */
-	public function __construct(Response $response, Language $lang)
+	public function __construct(Response $response, Request $request, Language $lang)
 	{
 		parent::__construct($response);
 
+		$this->request = $request;
 		$this->lang = $lang;
 		$this->response->lang = $lang;
 	}
